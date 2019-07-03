718edaeff6e63eceddab99d70506f44a3c2d1baf
Daniel Wolf <daniel.wolf1@volkswagen.de>
2019-07-03 13:46:07 +0200

    kkk

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-	res.send("hi");
+	res.send("jooooooooooo");
 });
 
 app.listen(port, () => {
